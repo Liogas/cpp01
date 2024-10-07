ce09c728964e358ce6e1fcee0af47ee9268c2e4e
Gaston Lions <glions@paul-f4Br7s5.clusters.42paris.fr>
2024-10-07 09:35:16 +0200

    finit

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: glions <glions@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 22:25:21 by glions            #+#    #+#             */
-/*   Updated: 2024/09/27 15:05:12 by glions           ###   ########.fr       */
+/*   Updated: 2024/10/07 09:25:26 by glions           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int main(void)
     int         horde_size;
     std::string horde_name;
 
-    horde_size = -1;
+    horde_size = 50;
     horde_name = "horde1";
     horde = zombieHorde(horde_size, horde_name);
     if (!horde)
